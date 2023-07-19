956dfacebca571d7840db81dcbb15d4a65035f40
Raghad abu-mansour <raghad.m7md.1417@gmail.com>
2023-07-19 08:20:21 +0300

    commit first v1.0.0

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    baseURL:'http://ahmadali11-001-site1.etempurl.com/api/'
+    baseURL:'https://beautyhubdev.azurewebsites.net/api/'
+   
   };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseURL:'http://ahmadali11-001-site1.etempurl.com/api/'
+  baseURL:'https://beautyhubdev.azurewebsites.net/api/'
 };
