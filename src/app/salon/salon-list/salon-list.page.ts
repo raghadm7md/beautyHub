52383ba026b7d6abe8197ed3594b5fcf54fafb0b
Raghad abu-mansour <raghad.m7md.1417@gmail.com>
2023-10-08 22:24:01 +0300

    feat(search): add search in saloon and service by names related to #14

--- src/app/salon/salon-list/salon-list.page.ts
+++ src/app/salon/salon-list/salon-list.page.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoadingController } from '@ionic/angular';
 import { SalonServiceService } from '../salon-service.service';
 import { ISalon } from '../salon.model';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-salon-list',
@@ -8,14 +12,47 @@ import { ISalon } from '../salon.model';
   styleUrls: ['./salon-list.page.scss'],
 })
 export class SalonListPage implements OnInit {
-  salons:ISalon[]
+  salons: ISalon[]
+  searchForm: FormGroup
+  private searchSubject = new Subject<string>();
+  inputText: string = '';
+  private readonly debounceTimeMs = 1000;
 
-  constructor(private _salonservice: SalonServiceService ) { }
+  constructor(private _salonservice: SalonServiceService,
+    private loadingCtrl: LoadingController,
+  ) { }
 
-  ngOnInit() {
-    this._salonservice.fetchsalonList().subscribe(salonList=>{
-    this.salons=salonList
+  async ngOnInit() {
+    let loading = await this.loadingCtrl.create();
+    loading.present();
+    this._salonservice.fetchsalonList().subscribe(salonList => {
+      this.salons = salonList
+      loading.dismiss()
     })
+
+    this.searchSubject.pipe(debounceTime(this.debounceTimeMs)).subscribe((searchValue) => {
+      this.performSearch(searchValue);
+    });
+
+  }
+
+  ngOnDestroy() {
+    this.searchSubject.complete();
+  }
+
+  onSearch() {
+    this.searchSubject.next(this.inputText);
+  }
+
+  async performSearch(searchValue: string) {
+    let loading = await this.loadingCtrl.create();
+    loading.present();
+    this._salonservice.fetchSalonOnsearch(searchValue)
+      .subscribe(searchResult => {
+        this.salons = searchResult
+        loading.dismiss()
+      });
+
   }
 
 }
